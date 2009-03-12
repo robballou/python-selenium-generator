27ccb3e6b6ae9b2e740b5173cdec176cf63c93fb
Rob Ballou <rballou@RobMac2.local>
2009-03-12 09:42:43 -0500

    Created the contrib directory with a few textmate commands.

---
+++ contrib/textmate_create_command.sh
@@ -0,0 +1,18 @@
+#
+# TextMate command for calling the Selenium test code
+#
+# Use the selenium test generator to create a test for a form. This
+# command will prompt for the URL of the form. If you want to create
+# a test case off of the current document, see the 'textmate_create_command.sh'
+#
+# Install: create a new command and set the values to:
+#
+#    Save: Nothing
+#    Command: (this code)
+#    Input: Entire Document
+#    Output: Create New Document
+#
+# Version: 0.1
+#
+
+cat $TM_FILEPATH | python $SELENIUM_GENERATOR/example.py --stdin
