1d647202f543b9a6908e60f5ccd0d182d8d59049
Rob Ballou <rballou@RobMac2.local>
2009-03-12 09:42:04 -0500

    Added a new option to the example script that allows use of the STDIN rather than a URL. Now you should be able to pass an HTML doc via STDIN to the script to get form data.

--- example.py
+++ example.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
-from python_selenium_generator import readurl, is_url, TestGenerator
+from python_selenium_generator import readhtml, readurl, is_url, TestGenerator
 import optparse
 import sys
 
 parser = optparse.OptionParser(usage='usage: %prog [options] url')
 parser.add_option('-j', '--json', action='store_true', dest='json', default=False, help="Return JSON representation of the form instead of the Selenium code")
+parser.add_option('--stdin', action='store_true', dest='stdin', default=False, help="Read data from STDIN rather than a URL")
 (options, args) = parser.parse_args()
 
-if len(args) != 1:
+if len(args) != 1 and not options.stdin:
     parser.error("Must provide a URL")
 
-if is_url(args[0]):
+if options.stdin:
+    page = sys.stdin
+    forms = readhtml.get_forms(page)
+    for form in forms:
+        if not options.json:
+            test = TestGenerator(form)
+            print test.create()
+        else:
+            print form.to_json()
+elif is_url(args[0]):
     # read web page and generate test
     page = readurl.get_page(args[0])
     forms = readurl.get_forms(page)
