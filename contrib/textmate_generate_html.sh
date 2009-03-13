42256c067759fb519c9af663aa8ff3e574020514
Rob Ballou <rballou@RobMac2.local>
2009-03-13 10:27:37 -0500

    Added new Textmate command for generating the Selenese HTML output.

---
+++ contrib/textmate_generate_html.sh
@@ -0,0 +1,29 @@
+#
+# TextMate command for calling the Selenium test code
+#
+# Use the selenium test generator to create a selenese test for a form. This
+# command will prompt for the URL of the form. If you want to create
+# a test case off of the current document, see the 'textmate_create_command.sh'
+#
+# Install: create a new command and set the values to:
+#
+#    Save: Nothing
+#    Command: (this code)
+#    Input: None
+#    Output: Insert as Text
+#
+# You also need to set $SELENIUM_GENERATOR in your TM preferences (Advanced -> Shell Variables) 
+#
+# Version: 0.1
+#
+
+# get URL
+res=$(CocoaDialog inputbox --title "Form URL" --informative-text "Please enter the URL for the form:" --button1 "Go" --button2 "Cancel")
+
+# handle the cancel
+[[ $(head -n1 <<<"$res") == 2 ]] && exit_discard
+
+# format the URL
+url=$(tail -n1 <<<"$res")
+
+python $SELENIUM_GENERATOR/example.py --html $url
